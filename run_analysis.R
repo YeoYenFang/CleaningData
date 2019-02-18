88522406d02945e9fcff377f2e93b3a7fa80681c
YeoYenFang <46638198+YeoYenFang@users.noreply.github.com>
2019-02-19 00:25:12 +0800

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,57 @@
+
+# set working directory
+> setwd (./coursera/UCI HAR Dataset)
+
+
+# download the data
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL, filename="W4project.zip", method="curl")
+
+# load packages
+Library(data.table)
+
+# read in the data
+activities <- read.table("activity_labels.txt", col.names = c("code", "activity"))
+features <- read.table("features.txt", col.names = c("sn","functions"))
+
+subject_test <- readtable("test/subject_test.txt", col.names = "subject")
+x_test <- read.table("test/X_test.txt", col.names = features$functions)
+y_test <- read.table("test/y_test.txt", col.names = "code")
+
+subject_train <- read.table("train/subject_train.txt", col.names = "subject")
+x_train <- read.table("train/X_train.txt", col.names = features$functions)
+y_train <- read.table("train/y_train.txt", col.names = "code")
+
+
+# all subjects - Merge
+allSubject <- rbind(subject_train, subject_test)
+allX <- rbind(x_train, x_test)
+allY <- rbind(y_train, y_test)
+alldata <- cbind(allSubject, allY, allX)
+
+# extract variables for mean and standard deviation for each type of measurement
+SelectedItem_Tidy <- select(alldata, code, contains("subjects"), contains("mean"), contains("std"))
+
+# use activies description instead of code
+SelectedItem_Tidy$code <- activities[SelectedItem_Tidy$code, 2]
+
+# label the data items
+names(SelectedItem_Tidy)[1] = "activity"
+names(SelectedItem_Tidy)<-gsub("Acc", "Accelerometer", names(SelectedItem_Tidy))
+names(SelectedItem_Tidy)<-gsub("Gyro", "Gyroscope", names(SelectedItem_Tidy))
+names(SelectedItem_Tidy)<-gsub("BodyBody", "Body", names(SelectedItem_Tidy))
+names(SelectedItem_Tidy)<-gsub("Mag", "Magnitude", names(SelectedItem_Tidy))
+names(SelectedItem_Tidy)<-gsub("^t", "Time", names(SelectedItem_Tidy))
+names(SelectedItem_Tidy)<-gsub("^f", "Frequency", names(SelectedItem_Tidy))
+names(SelectedItem_Tidy)<-gsub("tBody", "TimeBody", names(SelectedItem_Tidy))
+names(SelectedItem_Tidy)<-gsub("-mean()", "Mean", names(SelectedItem_Tidy))
+names(SelectedItem_Tidy)<-gsub("-std()", "STD", names(SelectedItem_Tidy))
+names(SelectedItem_Tidy)<-gsub("-freq()", "Frequency", names(SelectedItem_Tidy))
+
+# summary dataset
+SummaryData <- SelectedItem_Tidy %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+
+# output the data
+write.table(SummaryData, file = "SummaryData.txt")
